9d25a8bd89f1d0a714de88f671930e280a0b8aba
ThoKhang <khangheheqt@gmail.com>
2025-09-26 20:42:03 +0700

    hoan thien cac su kien

--- CK/app/src/main/java/com/example/musicapp/trangregister.java
+++ CK/app/src/main/java/com/example/musicapp/trangregister.java
@@ -2,7 +2,7 @@ package com.example.musicapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class trangregister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_trangregister);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button buttonRegister = findViewById(R.id.dangKyNgayBayGio320);
-        buttonRegister.setOnClickListener(
-                v -> {
-                    Intent intent = new Intent(this, trangregister.class);
-                    startActivity(intent);
-                }
-        );
+        TextView textView = findViewById(R.id.dangNhapNgayBayGio320);
+        textView.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
